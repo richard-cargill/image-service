37d95ecbc2ddc077071ebea570c8f0cec74a36e0
Richard Cargill <catdoce@gmail.com>
2017-09-17 03:50:10 +0100

    added in the ability to convert the file type

--- src/handler.js
+++ src/handler.js
@@ -3,21 +3,24 @@ const query = require('micro-query');
 const fetch = require('node-fetch');
 
 const endpoint = async (req, res) => {
-  const {u, w} = query(req);
-  if (typeof u !== 'undefined' || typeof w !== 'undefined') {
-    try {
-      res.writeHead(200, {'content-type': 'image/png'});
+  const {u, w, t} = query(req);
 
+  if (typeof u === 'undefined' || typeof w === 'undefined') {
+    res.end('');
+  } else {
+    try {
       const response = await fetch(u.toString());
       const buffer = await response.buffer();
-      const img = await sharp(buffer).resize(Number(w)).toBuffer();
+      const image = await sharp(buffer);
+      const meta = await image.metadata();
+      const format = t || meta.format;
+      const output = await image.resize(Number(w))[format]().toBuffer();
 
-      res.end(img, 'binary');
+      res.writeHead(200, {'content-type': `image/${format}`});
+      res.end(output, 'binary');
     } catch (err) {
       console.error(err);
     }
-  } else {
-    res.end();
   }
 };
 
